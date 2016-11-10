0622786d21a4a4e7aa1d0f8ab2a85af41a72369a
snifty <aleksanderne1@gmail.com>
2016-11-10 03:54:46 +0100

    Add files via upload

---
+++ Code/water_sensor.ino
@@ -0,0 +1,21 @@
+    const int VAL_PROBE = 0; // Analog pin 0
+    const int MOISTURE_LEVEL = 50; // the value after the LED goes ON
+    const int PUMP = 8;
+    
+    void setup() {
+        Serial.begin(9600);
+        pinMode(PUMP, OUTPUT);
+    }
+     
+    void loop() {
+        int moistureMap = map(analogRead(VAL_PROBE), 1023, 0, 0, 100);
+        Serial.print(moistureMap);
+        Serial.println("%");
+     
+        if(moistureMap < MOISTURE_LEVEL) {
+            digitalWrite(PUMP,HIGH);
+        }else{
+            digitalWrite(PUMP,LOW);
+        }
+        delay(500);
+    }
